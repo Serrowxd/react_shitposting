2fa66a0a7616aae422b340e90d295c51f8f87250
Kevin Jolley <serrowpls@gmail.com>
2018-07-23 20:00:06 -0500

    updated, broken with reactstrap

--- src/App.js
+++ src/App.js
@@ -1,97 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './App.css';
 
-import Nav from './nav/nav';
-import Breadcrumb from './nav/breadcrumb';
+import img from './assets/hand.jpeg';
+import img2 from './assets/itworks.png';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isOpen: true,
-      name: ['Kevin'],
-      postsAva: ['1'],
-      freeCall: ['3'],
-      credits: ['20'],
-      crumbs: ['(OvO") /'],
-    };
-
-    this.toggle = this.toggle.bind(this);
-    this.bCrumb = this.bCrumb.bind(this);
-    this.unCrumb = this.unCrumb.bind(this);
-  }
-
-  bCrumb() {
-    const joined = this.state.crumbs.concat('Henlo (OvO") /');
-    this.setState({ crumbs: joined });
-  }
-
-  unCrumb() {
-    const unjoined = this.state.crumbs.splice(1);
-    this.setState({ crumbs: unjoined });
-  }
-
-  toggle() {
-    const currentState = this.state.isOpen;
-    this.setState({ isOpen: !currentState });
-  }
-
-  render() {
-    const open = this.state.isOpen;
-    return (
-      <div class="nav_container">
-        <div class="cont">
-          <div>
-            <Breadcrumb crumb={this.state.crumbs} />
-          </div>
-          <div class="navContainer">
-            <Nav
-              name={this.state.name}
-              postsAva={this.state.postsAva}
-              freeCall={this.state.freeCall}
-              credits={this.state.credits}
-            />
-            <h4
-              class={open ? 'collapse' : 'collapse_open'}
-              onClick={this.toggle}
-            >
-              |||
-            </h4>
-          </div>
-        </div>
-        <div
-          class={open ? 'nav_collapse' : 'nav_collapse_open'}
-          // onClick={this.toggle} // this toggles the auto-close
-        >
-          <a href="#" class="nav_link">
-            <h3> Profile </h3>
-          </a>
-          <a href="#" class="nav_link">
-            <h3> Matches </h3>
-          </a>
-          <a href="#" class="nav_link">
-            <h3> Messages </h3>
-          </a>
-
-          <a href="#" class="nav_link">
-            <h3>Job Postings </h3>
-          </a>
-          <a href="#" class="nav_link">
-            <h3> Billing </h3>
-          </a>
-
-          <hr />
-          <a href="#" class="nav_link">
-            <h3> Sign Out </h3>
-          </a>
-        </div>
-        <button onClick={this.bCrumb}> Click Me (OvO") </button>
-        <button onClick={this.unCrumb}> Don't Click Me (O^O") </button>
+const App = () => {
+  return (
+    <div class="bkg_img">
+      <img src={img} class="img1" />
+      <div class="bro_bigtext">
+        <h1> Job Me Bro! </h1>
+        <h4> Our Proven System WORKS!!! </h4>
+        <img src={img2} class="img2" />
       </div>
-    );
-  }
-}
+      <div class="bro_signup_cont">
+        <div class="bro_signup">
+          <h1> Sign Up Today! </h1>
+          <button class="ripple"> I'm a Seeker </button>
+          <button class="ripple"> I'm a Recruiter </button>
+          <p> Already have an account?</p>
+          <button class="ripple"> Log-In </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/nav/App.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+import Nav from './nav';
+import Breadcrumb from './breadcrumb';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: true,
+      name: ['Kevin'],
+      postsAva: ['1'],
+      freeCall: ['3'],
+      credits: ['20'],
+      crumbs: ['(OvO") /'],
+    };
+
+    this.toggle = this.toggle.bind(this);
+    this.bCrumb = this.bCrumb.bind(this);
+    this.unCrumb = this.unCrumb.bind(this);
+  }
+
+  bCrumb() {
+    const joined = this.state.crumbs.concat('Henlo (OvO") /');
+    this.setState({ crumbs: joined });
+  }
+
+  unCrumb() {
+    const unjoined = this.state.crumbs.splice(1);
+    this.setState({ crumbs: unjoined });
+  }
+
+  toggle() {
+    const currentState = this.state.isOpen;
+    this.setState({ isOpen: !currentState });
+  }
+
+  render() {
+    const open = this.state.isOpen;
+    return (
+      <div class="nav_container">
+        <div class="cont">
+          <div>
+            <Breadcrumb crumb={this.state.crumbs} />
+          </div>
+          <div class="navContainer">
+            <Nav
+              name={this.state.name}
+              postsAva={this.state.postsAva}
+              freeCall={this.state.freeCall}
+              credits={this.state.credits}
+            />
+            <h4
+              class={open ? 'collapse' : 'collapse_open'}
+              onClick={this.toggle}
+            >
+              |||
+            </h4>
+          </div>
+        </div>
+        <div
+          class={open ? 'nav_collapse' : 'nav_collapse_open'}
+          // onClick={this.toggle} // this toggles the auto-close
+        >
+          <a href="#" class="nav_link">
+            <h3> Profile </h3>
+          </a>
+          <a href="#" class="nav_link">
+            <h3> Matches </h3>
+          </a>
+          <a href="#" class="nav_link">
+            <h3> Messages </h3>
+          </a>
+
+          <a href="#" class="nav_link">
+            <h3>Job Postings </h3>
+          </a>
+          <a href="#" class="nav_link">
+            <h3> Billing </h3>
+          </a>
+
+          <hr />
+          <a href="#" class="nav_link">
+            <h3> Sign Out </h3>
+          </a>
+        </div>
+        <button onClick={this.bCrumb}> Click Me (OvO") </button>
+        <button onClick={this.unCrumb}> Don't Click Me (O^O") </button>
+      </div>
+    );
+  }
+}
+
+export default App;
