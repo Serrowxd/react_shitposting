52947eac72b30ae4214e95a2edd93f9a1c7fae18
Kevin Jolley <serrowpls@gmail.com>
2019-07-03 05:10:41 -0500

    refactored my old code to be more professional

--- src/stickynav/App.js
+++ src/stickynav/App.js
@@ -1,21 +1,18 @@
+// Refactored Code
+
 import React, { Component } from 'react';
 import './styles.css';
+// classList .add / .remove
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      scrolled: false,
-    };
-  }
-
   componentDidMount() {
     window.addEventListener('scroll', () => {
-      const isTop = window.scrollY < 100;
-      if (isTop !== true) {
-        this.setState({ scrolled: true });
+      const isTop = window.scrollY > 100;
+      const nav = document.getElementById('nav');
+      if (isTop) {
+        nav.classList.add('scrolled');
       } else {
-        this.setState({ scrolled: false });
+        nav.classList.remove('scrolled');
       }
     });
   }
@@ -26,7 +23,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className={this.state.scrolled ? 'nav scrolled' : 'nav'}>
+      <div id="nav" className="nav">
         <div className="nav_text">
           <h1>Henlo (OvO")</h1>
           <h1>I'm a navigation text!</h1>
@@ -37,3 +34,44 @@ class App extends Component {
 }
 
 export default App;
+
+// Old Code
+// import React, { Component } from 'react';
+// import './styles.css';
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       scrolled: false,
+//     };
+//   }
+
+//   componentDidMount() {
+//     window.addEventListener('scroll', () => {
+//       const isTop = window.scrollY < 100;
+//       if (isTop !== true) {
+//         this.setState({ scrolled: true });
+//       } else {
+//         this.setState({ scrolled: false });
+//       }
+//     });
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('scroll');
+//   }
+
+//   render() {
+//     return (
+//       <div className={this.state.scrolled ? 'nav scrolled' : 'nav'}>
+//         <div className="nav_text">
+//           <h1>Henlo (OvO")</h1>
+//           <h1>I'm a navigation text!</h1>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
